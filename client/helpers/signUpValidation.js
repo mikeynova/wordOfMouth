4e1df6b7b785a83f9fc5f355bb6d19590e33cf95
Michael Terranova <newfoundland7@gmail.com>
2017-01-06 16:52:37 -0800

    [WIP] Added regex validation

--- client/helpers/signUpValidation.js
+++ client/helpers/signUpValidation.js
@@ -1,18 +1,24 @@
 exports.first = function(first) {
-	console.log(first + ': ' + 'first validation function')
+	// console.log(first + ': ' + 'first validation function')
+	// if(first === "") {
+	// 	alert('wrong')
+	// }
+	// must be letters only
+	console.log(/[a-zA-Z\s:]/.test(first));
+	// document.prepend() the error to something bellow the text box
 }
 
 exports.last = function(last) {
-	console.log(last + ': ' + 'last validation function')
+	// console.log(last + ': ' + 'last validation function')
 }
 
 exports.email = function(email) {
-	console.log(email + ': ' + 'email validation function')
+	// console.log(email + ': ' + 'email validation function')
 }
 
 exports.password = function(password) {
-	console.log(password + ': ' + 'password validation funciton')
+	// console.log(password + ': ' + 'password validation funciton')
 }
 exports.repeatPassword = function(repeatPassword) {
-	console.log(repeatPassword + ': ' + 'repeatPassword validation function')
+	// console.log(repeatPassword + ': ' + 'repeatPassword validation function')
 }
