e8e4511faa5b9450fd6e9fd296340b05f64db54f
Michael Terranova <newfoundland7@gmail.com>
2017-02-24 17:50:33 -0800

    Pulling Network data with phantomjs and scraping for likes url

--- client/components/signUp.js
+++ client/components/signUp.js
@@ -5,6 +5,7 @@ import { actions } from '../actions/signUpValidationActions.js';
 import ReactTransitionGroup from 'react-addons-transition-group';
 import injectSheet from 'react-jss';
 import axios from 'axios';
+import network from '../server/soundcloud.js';
 
 const styles = {
   inputBox: {
@@ -175,6 +176,7 @@ class SignUp extends Component {
 
   componentDidMount()  {
   	// axios.get('/getSound')
+  	console.log(network);
   }
 
 	onChangeFirst(e) {
