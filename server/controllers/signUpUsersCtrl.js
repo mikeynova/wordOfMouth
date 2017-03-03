8fec5a1c202b4acda9d58dc9644a760432910b7f
mikeynova <newfoundland7@gmail.com>
2017-03-02 20:41:49 -0800

    merge conflicts incoming

--- server/controllers/signUpUsersCtrl.js
+++ server/controllers/signUpUsersCtrl.js
@@ -11,24 +11,21 @@ module.exports = {
 		 //  });
 			const phantom = require('phantom'); 
 			(async function() {
-					var flag = true;
-			    const instance = await phantom.create();
-			    const page = await instance.createPage();
-			    await page.on("onResourceRequested", function(requestData) {
-		        let found = requestData.url.search('likes');
-						  if(found !== -1 && found < 50 && requestData.url.length > 40 && flag) {
-						    flag = false;
-						    res.send(requestData.url);
-						  }
-			    });
-			 
-			    const status = await page.open('https://soundcloud.com/mikeynova/likes');
-			    // console.log(status);
-			 
-			    const content = await page.property('content');
-			    // console.log(content);
-			 
-			    await instance.exit();
+				var flag = true;
+		    const instance = await phantom.create();
+		    const page = await instance.createPage();
+		    await page.on("onResourceRequested", function(requestData) {
+	        let found = requestData.url.search('likes');
+					  if(found !== -1 && found < 50 && requestData.url.length > 40 && flag) {
+					    flag = false;
+					    res.send(requestData.url);
+					  }
+		    });
+		    const status = await page.open('https://soundcloud.com/mikeynova/likes');
+		    // console.log(status);
+		    const content = await page.property('content');
+		    // console.log(content);
+		    await instance.exit();
 			}());
 		},
 		post: (req, res) => {
